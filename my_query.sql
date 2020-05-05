feaedac1b93cc821ff83735beec03ebea4b2e9b1
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-05-05 08:24:58 +0700

    add my_query

---
+++ my_query.sql
@@ -0,0 +1,14 @@
+SELECT productCode, productName, buyprice, quantityInStock 
+FROM products
+WHERE buyprice > 56.76 and quantityInStock > 10;
+
+SELECT productCode,productName, buyprice, textDescription
+FROM products
+INNER JOIN productlines
+ON products.productline = productlines.productline
+WHERE buyprice > 56.76 AND buyprice < 95.59;
+
+SELECT productCode, productName, buyprice, quantityInStock, productVendor, productLine 
+FROM products 
+WHERE productLine = 'Classic Cars' or productVendor = 'Min Lin Diec';
+
